d460060c6b184ed8454d4a1ae8b2aba3eed3f3c2
Brendan Ralston <ralston.brendan@me.com>
2019-03-10 20:18:21 -0600

    Add non-default identity configuration

--- SunridgeHOA/Startup.cs
+++ SunridgeHOA/Startup.cs
@@ -37,8 +37,11 @@ namespace SunridgeHOA
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection"))); // Change to "TitanDB" when ready
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
