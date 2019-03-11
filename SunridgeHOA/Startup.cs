d24ed62186d51d0c78c499bb2afc735ef3d2a44b
Brendan Ralston <ralston.brendan@me.com>
2019-03-10 20:20:09 -0600

    Enable user sessions

--- SunridgeHOA/Startup.cs
+++ SunridgeHOA/Startup.cs
@@ -44,6 +44,12 @@ namespace SunridgeHOA
                 .AddDefaultTokenProviders();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            })
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +71,7 @@ namespace SunridgeHOA
             app.UseCookiePolicy();
 
             app.UseAuthentication();
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
