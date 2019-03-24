7dd5bd317afc579670458172cf7326d0cd9792db
Brendan Ralston <ralston.brendan@me.com>
2019-03-23 18:55:59 -0600

    Removed username from Application User

--- SunridgeHOA/Models/ApplicationUser.cs
+++ SunridgeHOA/Models/ApplicationUser.cs
@@ -10,8 +10,6 @@ namespace SunridgeHOA.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string UserName { get; set; }
-
         public string UserPassword { get; set; }
 
         public string UserType { get; set; }
