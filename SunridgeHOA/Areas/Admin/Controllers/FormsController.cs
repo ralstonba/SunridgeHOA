d6be00972698b90d6d7e48deeb61cf4497418c7d
brocksmith24 <brocksmith38@gmail.com>
2019-03-31 17:21:57 -0600

    Added first few crud functionality to Forms

---
+++ SunridgeHOA/Areas/Admin/Controllers/FormsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SunridgeHOA.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FormsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePOST() //Don't need to pass datamodel because of binding
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
