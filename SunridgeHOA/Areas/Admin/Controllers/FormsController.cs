888265141d8e96e15767c05b9e149c671f050a45
brocksmith24 <brocksmith38@gmail.com>
2019-03-31 21:05:10 -0600

    Maps and Forms change

--- SunridgeHOA/Areas/Admin/Controllers/FormsController.cs
+++ SunridgeHOA/Areas/Admin/Controllers/FormsController.cs
@@ -1,29 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SunridgeHOA.Data;
+using SunridgeHOA.Models;
+using SunridgeHOA.Models.ViewModels;
 
 namespace SunridgeHOA.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class FormsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
+
+        private string WebRootPath { get; set; }
+
+        public FormsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+            WebRootPath = hostingEnvironment.WebRootPath;
+        }
+
+        public IActionResult CreateForms()
         {
             return View();
         }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost, ActionName("Create")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreatePOST() //Don't need to pass datamodel because of binding
+        public async Task<IActionResult> CreateFormsPOST()
         {
-            return RedirectToAction(nameof(Index));
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Forms", "Home");
+            }
+            //_db.Files.Add();
+            await _db.SaveChangesAsync();
+
+            var files = HttpContext.Request.Form.Files;
+
+            if (files.Count != 0)
+            {
+                var uploads = Path.Combine(WebRootPath, @"img\Maps");
+                var extension = Path.GetExtension(files[0].FileName);
+                //using (var filestream = new FileStream(Path.Combine(uploads, map.ID + extension), FileMode.Create))
+                //{
+                //    files[0].CopyTo(filestream);
+                //}
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Forms", "Home");
         }
     }
 }
---
+++ SunridgeHOA/Areas/Admin/Controllers/MapsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using SunridgeHOA.Data;
+using SunridgeHOA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunridgeHOA.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MapsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
+
+        private string WebRootPath { get; set; }
+        public Map map { get; set; }
+        public MapsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+            WebRootPath = hostingEnvironment.WebRootPath;
+            map = new Map();
+        }
+
+        public IActionResult AddMap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMapPOST()
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Maps", "Home");
+            }
+
+            _db.Map.Add(map);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Maps", "Home");
+        }
+    }
+}
