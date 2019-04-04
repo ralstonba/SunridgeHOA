618299577a7777ef4f4d5976a6475e680b9474ae
AaronNybo <aaron.nybo@gmail.com>
2019-04-03 21:58:48 -0600

    Introduced basic View for Admin Panel

---
+++ SunridgeHOA/Areas/Admin/Controllers/PanelController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SunridgeHOA.Data;
+
+namespace SunridgeHOA.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PanelController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PanelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
