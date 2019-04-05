9555311aafc4b1b75cbaacca9d475b46b723a19b
Brendan Ralston <ralston.brendan@me.com>
2019-04-05 11:54:24 -0600

    Add MyLots controller to Owners Area

---
+++ SunridgeHOA/Areas/Owners/Controllers/MyLotsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SunridgeHOA.Areas.Owners.Controllers
+{
+    [Area("Owners")]
+    public class MyLotsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
