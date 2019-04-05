56e8fce712217bad92dfbd48299e5aefbe8b77dd
Brendan Ralston <ralston.brendan@me.com>
2019-04-05 12:17:23 -0600

    Added viewmodel for necessary info

---
+++ SunridgeHOA/Models/ViewModels/MyLotsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunridgeHOA.Models.ViewModels
+{
+    public class MyLotsViewModel
+    {
+        public int OwnerID { get; set; }
+        public IEnumerable<Lot> Lots { get; set; }
+    }
+}
