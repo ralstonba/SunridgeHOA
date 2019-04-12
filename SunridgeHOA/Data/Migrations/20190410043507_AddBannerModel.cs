5cd7c981817cf67fc05e26760e785b9a096875ac
brocksmith24 <brocksmith38@gmail.com>
2019-04-11 18:08:13 -0600

    Additions for Admin Edit Banner Functionality

---
+++ SunridgeHOA/Data/Migrations/20190410043507_AddBannerModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SunridgeHOA.Data.Migrations
+{
+    public partial class AddBannerModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banner",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Header = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banner", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banner");
+        }
+    }
+}
