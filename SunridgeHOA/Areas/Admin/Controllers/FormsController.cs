35add8790a2a7a95bfb156186db294615e7406f2
brocksmith24 <brocksmith38@gmail.com>
2019-04-11 20:52:02 -0600

    Beginning Forms Commit

---
+++ SunridgeHOA/Areas/Admin/Controllers/FormsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SunridgeHOA.Data;
+using SunridgeHOA.Models;
+
+namespace SunridgeHOA.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FormsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
+
+        public FormsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
