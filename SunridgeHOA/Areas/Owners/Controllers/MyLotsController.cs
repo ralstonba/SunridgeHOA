5f339c0a2bf56901b30e042f2e8b0672cb9a6483
Brendan Ralston <ralston.brendan@me.com>
2019-04-12 20:47:22 -0600

    Fix NPE on my lots controller and add my lots to admin panel

--- SunridgeHOA/Areas/Owners/Controllers/MyLotsController.cs
+++ SunridgeHOA/Areas/Owners/Controllers/MyLotsController.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SunridgeHOA.Data;
+using SunridgeHOA.Models;
 using SunridgeHOA.Models.ViewModels;
+using SunridgeHOA.Utility;
 
 namespace SunridgeHOA.Areas.Owners.Controllers
 {
+    [Authorize(Roles = StaticData.OwnerEndUser)]
     [Area("Owners")]
     public class MyLotsController : Controller
     {
@@ -22,12 +26,15 @@ namespace SunridgeHOA.Areas.Owners.Controllers
         {
             _db = db;
             _userManager = userManager;
+
+            viewModel = new MyLotsViewModel();
         }
 
         public IActionResult Index()
         {
             viewModel.OwnerID = _db.ApplicationUsers.Include(m => m.Owner)
                 .FirstOrDefault(m => m.Id.Equals(_userManager.GetUserId(HttpContext.User))).Owner.ID;
+
             viewModel.Lots = _db.Lots.Where(m => m.OwnerID == viewModel.OwnerID).ToList();
 
             return View(viewModel);
